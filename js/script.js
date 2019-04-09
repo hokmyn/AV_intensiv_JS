1c8c9e4d149a63bcf78911b0d9920084960228a1
Denis <den_from_bykhov1988@mail.ru>
2019-04-10 01:48:08 +0300

    Второе дз

--- js/script.js
+++ js/script.js
@@ -90,8 +90,84 @@ const data = [
             </div>
         `;
         videosWrapper.appendChild(card);
+        if (night === true) {
+            document.querySelectorAll('.videos__item-descr').forEach(item => {
+                item.style.color = '#fff';
+            });
+            document.querySelectorAll('.videos__item-views').forEach(item => {
+                item.style.color = '#fff';
+            });
+        }
         setTimeout(() => {
             card.classList.remove('videos__item-active');
         }, 10);
+        bindNewModal(card);
     }
+
+    sliceTitle('.videos__item-descr', 100);
   });
+
+  function sliceTitle(selector, count) {
+      document.querySelectorAll(selector).forEach(item => {
+        item.textContent.trim();
+        if(item.textContent.length < 100) {
+            return;
+        } else {
+            const str = item.textContent.slice(0, count + 1) + "...";
+            item.textContent = str;
+        }
+      });
+  }
+  sliceTitle('.videos__item-descr', 100);
+
+  function openModal() {
+      modal.style.display = 'block';
+  }
+
+  function closeModal() {
+    modal.style.display = 'none';
+    player.stopVideo();
+  }
+
+  function bindModal(cards) {
+      cards.forEach(item => {
+          item.addEventListener('click', (e) => {
+              e.preventDefault();
+              const id = item.getAttribute('data-url');
+              loadVideo(id);
+              openModal();
+          });
+      });
+  }
+  bindModal(videos);
+  function bindNewModal(cards) {
+    cards.addEventListener('click', (e) => {
+        e.preventDefault();
+        openModal();
+    });
+}
+modal.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('modal__body')) {
+        closeModal();
+    }
+});
+
+function createVIdeo() {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    setTimeout(() => {
+        player = new YT.Player('frame', {
+            height: '100%',
+            width: '100%',
+            videoId: 'M7lc1UVf-VE'
+        });
+    }, 300);
+}
+createVIdeo();
+
+function loadVideo(id) {
+    player.loadVideoById({'videoId': `${id}`});
+}
+
